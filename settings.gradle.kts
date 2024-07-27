d918123e97a3ed2f7233681eef5887a67350ec0e
Taoning Ge <gtn1024@foxmail.com>
2024-07-27 23:08:14 +0800

    chore: enable gradle public scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,16 @@ pluginManagement {
         id("io.quarkus") version "3.12.3"
     }
 }
+
+plugins {
+    id("com.gradle.develocity") version("3.17.5")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
+
 rootProject.name = "quarkus-blog"
