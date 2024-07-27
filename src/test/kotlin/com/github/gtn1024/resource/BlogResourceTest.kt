30596cde21827a974650efdae7533224fe2f4a50
Taoning Ge <gtn1024@foxmail.com>
2024-07-27 21:40:43 +0800

    chore: make ktlint happy

--- src/test/kotlin/com/github/gtn1024/resource/BlogResourceTest.kt
+++ src/test/kotlin/com/github/gtn1024/resource/BlogResourceTest.kt
@@ -8,11 +8,9 @@ import io.restassured.common.mapper.TypeRef
 import jakarta.inject.Inject
 import jakarta.persistence.EntityManager
 import jakarta.transaction.Transactional
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.UUID
@@ -33,28 +31,29 @@ class BlogResourceTest {
         // truncate table
         entityManager.createNativeQuery("TRUNCATE TABLE t_blog").executeUpdate()
 
-        data = listOf(
-            BlogEntity().apply {
-                title = "title1"
-                content = "content1"
-            },
-            BlogEntity().apply {
-                title = "title2"
-                content = "content2"
-            },
-            BlogEntity().apply {
-                title = "title3"
-                content = "content3"
-            },
-            BlogEntity().apply {
-                title = "title4"
-                content = "content4"
-            },
-            BlogEntity().apply {
-                title = "title5"
-                content = "content5"
-            },
-        )
+        data =
+            listOf(
+                BlogEntity().apply {
+                    title = "title1"
+                    content = "content1"
+                },
+                BlogEntity().apply {
+                    title = "title2"
+                    content = "content2"
+                },
+                BlogEntity().apply {
+                    title = "title3"
+                    content = "content3"
+                },
+                BlogEntity().apply {
+                    title = "title4"
+                    content = "content4"
+                },
+                BlogEntity().apply {
+                    title = "title5"
+                    content = "content5"
+                },
+            )
 
         // insert data
         data.forEach { it.persist() }
@@ -77,7 +76,7 @@ class BlogResourceTest {
 
         given()
             .`when`()
-            .get("/blog/${id}")
+            .get("/blog/$id")
             .then()
             .statusCode(404)
     }
@@ -87,13 +86,14 @@ class BlogResourceTest {
         val id = data[0].id.toString()
         logger.info("id: {}", id)
 
-        val resp: Resp<BlogEntity> = given()
-            .`when`()
-            .get("/blog/${id}")
-            .then()
-            .statusCode(200)
-            .extract()
-            .`as`(object : TypeRef<Resp<BlogEntity>>() { })
+        val resp: Resp<BlogEntity> =
+            given()
+                .`when`()
+                .get("/blog/$id")
+                .then()
+                .statusCode(200)
+                .extract()
+                .`as`(object : TypeRef<Resp<BlogEntity>>() { })
 
         assertEquals(data[0].title, resp.data?.title)
         assertEquals(data[0].content, resp.data?.content)
@@ -101,36 +101,39 @@ class BlogResourceTest {
 
     @Test
     fun `list without pagination`() {
-        val resp: Resp<List<BlogEntity>> = given()
-            .`when`()
-            .get("/blog")
-            .then()
-            .statusCode(200)
-            .extract()
-            .`as`(object : TypeRef<Resp<List<BlogEntity>>>() { })
+        val resp: Resp<List<BlogEntity>> =
+            given()
+                .`when`()
+                .get("/blog")
+                .then()
+                .statusCode(200)
+                .extract()
+                .`as`(object : TypeRef<Resp<List<BlogEntity>>>() { })
 
         assertEquals(data.size, resp.data?.size)
     }
 
     @Test
     fun `list with pagination`() {
-        val resp: Resp<List<BlogEntity>> = given()
-            .`when`()
-            .get("/blog?page=1&pageSize=2")
-            .then()
-            .statusCode(200)
-            .extract()
-            .`as`(object : TypeRef<Resp<List<BlogEntity>>>() { })
+        val resp: Resp<List<BlogEntity>> =
+            given()
+                .`when`()
+                .get("/blog?page=1&pageSize=2")
+                .then()
+                .statusCode(200)
+                .extract()
+                .`as`(object : TypeRef<Resp<List<BlogEntity>>>() { })
 
         assertEquals(2, resp.data?.size)
     }
 
     @Test
     fun create() {
-        val data = mapOf(
-            "title" to "title6",
-            "content" to "content6",
-        )
+        val data =
+            mapOf(
+                "title" to "title6",
+                "content" to "content6",
+            )
 
         given()
             .contentType("application/json")
@@ -147,15 +150,16 @@ class BlogResourceTest {
     @Test
     fun patch() {
         val uuid = data[0].id!!
-        val data = mapOf(
-            "title" to "title6",
-        )
+        val data =
+            mapOf(
+                "title" to "title6",
+            )
 
         given()
             .contentType("application/json")
             .body(data)
             .`when`()
-            .patch("/blog/${uuid}")
+            .patch("/blog/$uuid")
             .then()
             .statusCode(200)
 
@@ -166,16 +170,17 @@ class BlogResourceTest {
     @Test
     fun put() {
         val uuid = data[0].id!!
-        val data = mapOf(
-            "title" to "title6",
-            "content" to "content6",
-        )
+        val data =
+            mapOf(
+                "title" to "title6",
+                "content" to "content6",
+            )
 
         given()
             .contentType("application/json")
             .body(data)
             .`when`()
-            .put("/blog/${uuid}")
+            .put("/blog/$uuid")
             .then()
             .statusCode(200)
 
@@ -190,7 +195,7 @@ class BlogResourceTest {
 
         given()
             .`when`()
-            .delete("/blog/${uuid}")
+            .delete("/blog/$uuid")
             .then()
             .statusCode(200)
 
